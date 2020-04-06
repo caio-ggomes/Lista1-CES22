f302137ea2e29adffe90a49741bf2fdfb0eaf42e
caio-ggomes <caio.graca@gmail.com>
2020-04-06 16:20:29 -0300

    question 1 done

---
+++ question1.py
@@ -0,0 +1,24 @@
+import turtle
+
+playground = turtle.Screen()
+playground.bgcolor("lightgreen")
+
+artist = turtle.Turtle()
+artist.color("violet", "violet")
+artist.pensize(4) 
+
+def draw_square(size, turt):
+    for i in range(4):
+        turt.forward(size)
+        turt.left(90)
+
+current_size = 20
+current_position = (0, 0)
+for i in range(5):
+    draw_square(current_size, artist)
+    current_size += 20
+    artist.penup()
+    artist.setpos(artist.xcor()-10, artist.ycor()-10)
+    artist.pendown()
+
+playground.mainloop()
